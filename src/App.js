45561ec567f2f2d09fdc6869db9e826f802a3d5c
Asmita mukherjee <asmita.mukherjee@cbnits.com>
2020-09-28 13:14:01 +0530

    tost add

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import logo from './logo.svg';
 import './App.css';
 import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
-
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer } from 'react-toastify';
 
 
 import Routing from './Routing/routing'
@@ -12,9 +13,11 @@ import Routing from './Routing/routing'
 export default class App extends React.Component {
   render(){
     return(
-     
-     
-      <Routing />
+      <React.Fragment>
+        <ToastContainer autoClose={2000} />
+       <Routing/>
+     </React.Fragment>
+      
       
     )
   }
