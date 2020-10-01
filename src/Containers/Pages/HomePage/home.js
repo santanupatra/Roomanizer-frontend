b8de5186b38cbd5599960939960c7324f61ff211
samapikacbnits <koushik@natitsolved.com>
2020-10-01 12:24:27 +0530

    loginheader

--- src/Containers/Pages/HomePage/home.js
+++ src/Containers/Pages/HomePage/home.js
@@ -2,18 +2,21 @@ import React,{useState} from 'react';
 import './style.css';
 import { Container, Row, Col, Navbar } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import Header from '../../Common/header';
+import LoginHeader from '../../Common/login-header';
 import Formsec from './form-sec';
 import Footer from '../../Common/footer';
 
 
 export default class Home extends React.Component {
+  
 
   render() {
     return (
       <div className="home">
         <div className="header">
-          <Header></Header>
+          <LoginHeader></LoginHeader>
             <div className="">
               <Container className="mb-3">
                 <Row className="align-items-center">
