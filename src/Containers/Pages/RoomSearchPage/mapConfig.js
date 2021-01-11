22e80a4338f990f879278f871d698bf088430782
Asmita mukherjee <asmita.mukherjee@cbnits.com>
2021-01-11 18:13:34 +0530

    map config change

--- src/Containers/Pages/RoomSearchPage/mapConfig.js
+++ src/Containers/Pages/RoomSearchPage/mapConfig.js
@@ -1,6 +1,6 @@
 
-//export const mapApiKey    = 'AIzaSyDDeMWe3wO5mhMCb-wyUC9UIYORSmKbROc';
-export const mapApiKey    = 'AIzaSyA5LrPhIokuSBO5EgKEcfu859gog6fRF8w';
+export const mapApiKey    = 'AIzaSyDDeMWe3wO5mhMCb-wyUC9UIYORSmKbROc';
+//export const mapApiKey    = 'AIzaSyA5LrPhIokuSBO5EgKEcfu859gog6fRF8w';
 export const googleMapURL = "https://maps.googleapis.com/maps/api/js?key="+mapApiKey;  
 export const defaultZoom  = 4.6;
 export const defaultCenter  = { lat: 35.1495343, lng: -90.0489801 };
